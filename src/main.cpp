5e990bac209eef9d63ec3a298c6e4a337686f9a5
Mangelari <MiguelAngelARI@proton.me>
2026-06-09 09:39:10 +0100

    added: msi msdv

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,11 @@ const char* ssid = SSID_casa;
 const char* password = PASSWORD_casa;
 
 //Contadores para calcular RMS
-unsigned long pitch_counter = 0;
-unsigned long roll_counter = 0;
-unsigned long accelH_counter = 0;
-unsigned long accelV_counter = 0;
+float pitch_sum = 0;
+float roll_sum = 0;
+float accelH_sum = 0;
+float accelV_sum = 0;
+unsigned long counter = 0;
 
 //Lectura de calibración cada: 5s
 unsigned long timer_last_calibration = 0;
@@ -103,9 +104,9 @@ void save_settings_nvmemory()
   preferences.putBytes("IP", &settings.IP, sizeof(settings.IP));
   preferences.putBytes("GW", &settings.GW, sizeof(settings.GW));
   preferences.putBytes("SUB", &settings.SUB, sizeof(settings.SUB));
-  preferences.putBytes("BNO055_SAMPLERATE", &settings.bno055_samplerate, sizeof(settings.bno055_samplerate));
-  preferences.putBytes("MB_TCP_SAMPLERATE", &settings.mb_tcp_samplerate, sizeof(settings.mb_tcp_samplerate));
-  preferences.putBytes("MQTT_SAMPLERATE", &settings.mqtt_samplerate, sizeof(settings.mqtt_samplerate));
+  preferences.putBytes("BNO055_SR", &settings.bno055_samplerate, sizeof(settings.bno055_samplerate));
+  preferences.putBytes("MB_TCP_SR", &settings.mb_tcp_samplerate, sizeof(settings.mb_tcp_samplerate));
+  preferences.putBytes("MQTT_SR", &settings.mqtt_samplerate, sizeof(settings.mqtt_samplerate));
   preferences.end();
 }
 void load_settings_nvmemory()
@@ -114,9 +115,9 @@ void load_settings_nvmemory()
   preferences.getBytes("IP", &settings.IP, sizeof(settings.IP));
   preferences.getBytes("GW", &settings.GW, sizeof(settings.GW));
   preferences.getBytes("SUB", &settings.SUB, sizeof(settings.SUB));
-  preferences.getBytes("BNO055_SAMPLERATE", &settings.bno055_samplerate, sizeof(settings.bno055_samplerate));
-  preferences.getBytes("MB_TCP_SAMPLERATE", &settings.mb_tcp_samplerate, sizeof(settings.mb_tcp_samplerate));
-  preferences.getBytes("MQTT_SAMPLERATE", &settings.mqtt_samplerate, sizeof(settings.mqtt_samplerate));
+  preferences.getBytes("BNO055_SR", &settings.bno055_samplerate, sizeof(settings.bno055_samplerate));
+  preferences.getBytes("MB_TCP_SR", &settings.mb_tcp_samplerate, sizeof(settings.mb_tcp_samplerate));
+  preferences.getBytes("MQTT_SR", &settings.mqtt_samplerate, sizeof(settings.mqtt_samplerate));
   preferences.end();
 }
 void save_calibration_nvmemory()
@@ -136,44 +137,42 @@ void load_calibration_nvmemory()
 }
 
 void get_data()
-{//falta reset del contador rms para evitar overflow
+{
   sensors_event_t orientationData, linearAccelData;
   bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
   bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  counter++;
   //Calculo de pitch
-  float pitch_offset_value = orientationData.orientation.y - BNO_data.pitch_offset;
-  float pitch_abs_value = abs(pitch_offset_value);
-  pitch_counter++;
-  BNO_data.pitch_actual = pitch_offset_value;
-  BNO_data.pitch_pico = max(pitch_abs_value, BNO_data.pitch_pico);
-  BNO_data.pitch_rms -= BNO_data.pitch_rms / pitch_counter;
-  BNO_data.pitch_rms += pitch_abs_value / pitch_counter;
+  float pitch_woffset_value = orientationData.orientation.y - BNO_data.pitch_offset;
+  float pitch_abs_value = abs(pitch_woffset_value);
+  BNO_data.pitch_actual = pitch_woffset_value;
+  if(abs(BNO_data.pitch_pico) < pitch_abs_value) BNO_data.pitch_pico = pitch_woffset_value;
+  pitch_sum += pitch_abs_value;
+  BNO_data.pitch_rms = pitch_sum / counter;
   //Calculo de roll
-  float roll_offset_value = orientationData.orientation.z - BNO_data.roll_offset;
-  float roll_abs_value = abs(roll_offset_value);
-  roll_counter++;
-  BNO_data.roll_actual = roll_offset_value;
-  BNO_data.roll_pico = max(roll_abs_value, BNO_data.roll_pico);
-  BNO_data.roll_rms -= BNO_data.roll_rms / roll_counter;
-  BNO_data.roll_rms += roll_abs_value / roll_counter;
+  float roll_woffset_value = orientationData.orientation.z - BNO_data.roll_offset;
+  float roll_abs_value = abs(roll_woffset_value);
+  BNO_data.roll_actual = roll_woffset_value;
+  if(abs(BNO_data.roll_pico) < roll_abs_value) BNO_data.roll_pico = roll_woffset_value;
+  roll_sum += roll_abs_value;
+  BNO_data.roll_rms = roll_sum / counter;
   //Calculo de aceleración horizontal
-  float accelH_offset_value = linearAccelData.acceleration.x - BNO_data.accelH_offset;
-  float accelH_abs_value = abs(accelH_offset_value);
-  accelH_counter++;
-  BNO_data.accelH_actual = accelH_offset_value;
-  BNO_data.accelH_pico = max(accelH_abs_value, BNO_data.accelH_pico);
-  BNO_data.accelH_rms -= BNO_data.accelH_rms / accelH_counter;
-  BNO_data.accelH_rms += accelH_abs_value / accelH_counter;
+  float accelH_woffset_value = linearAccelData.acceleration.x - BNO_data.accelH_offset;
+  float accelH_abs_value = abs(accelH_woffset_value);
+  BNO_data.accelH_actual = accelH_woffset_value;
+  if(abs(BNO_data.accelH_pico) < accelH_abs_value) BNO_data.accelH_pico = accelH_woffset_value;
+  accelH_sum += accelH_abs_value;
+  BNO_data.accelH_rms = accelH_sum / counter;
   //Calculo de aceleración vertical
-  float accelV_offset_value = linearAccelData.acceleration.y - BNO_data.accelV_offset;
-  float accelV_abs_value = abs(accelV_offset_value);
-  accelV_counter++;
-  BNO_data.accelV_actual = accelV_offset_value;
-  BNO_data.accelV_pico = max(accelV_abs_value, BNO_data.accelV_pico);
-  BNO_data.accelV_rms -= BNO_data.accelV_rms / accelV_counter;
-  BNO_data.accelV_rms += accelV_abs_value / accelV_counter;
-  //BNO_data.msi = 100 *(0.5 + abs()  (BNO_data.accelV_rms / 9.8) * abs(((2*M_PI)/settings.bno055_samplerate)));
-  BNO_data.msdv = BNO_data.accelV_rms * ((accelV_counter * settings.bno055_samplerate) / 1000);
+  float accelV_woffset_value = linearAccelData.acceleration.y - BNO_data.accelV_offset;
+  float accelV_abs_value = abs(accelV_woffset_value);
+  BNO_data.accelV_actual = accelV_woffset_value;
+  if(abs(BNO_data.accelV_pico) < accelV_abs_value) BNO_data.accelV_pico = accelV_woffset_value;
+  accelV_sum += accelV_abs_value;
+  BNO_data.accelV_rms = accelV_sum / counter;
+  BNO_data.msdv = sqrt(pow(BNO_data.accelV_rms, 2) * millis() / 1000);
+  BNO_data.msi = 100 * (1 - exp(-pow(BNO_data.msdv/0.2, 2)));
+  
 }
 
 void get_calibration()
@@ -193,12 +192,14 @@ void print_terminal()
   Serial.print("\n[RMS]     ");
   Serial.print("Pitch: " + String(BNO_data.pitch_rms) + " || Roll: " + String(BNO_data.roll_rms)
                 + " || AccelH: " + String(BNO_data.accelH_rms) + " || AccelV: " + String(BNO_data.accelV_rms));
+  Serial.print("\n[MSDV]    "   + String(BNO_data.msdv) );
+  Serial.print("\n[MSI]    "   + String(BNO_data.msi) );
   Serial.print("\n--------CALIBRACION---------");
   Serial.print("\nSys: " + String(BNO_calibration.sys) + " || Gyro: " + String(BNO_calibration.gyro)
                 + " || Accel: " + String(BNO_calibration.accel) + " || Mag: " + String(BNO_calibration.mag));
   Serial.print("\n--------SAMPLE RATES--------");
   Serial.print("\nBNO-055: " + String(settings.bno055_samplerate) + "ms || Modbus TCP: " + String(settings.mb_tcp_samplerate)
-                + "ms" || " MQTT: " + String(settings.mqtt_samplerate) + "ms");
+                + "ms ||  MQTT: " + String(settings.mqtt_samplerate) + "ms");
   Serial.print("\n----------SETTINGS----------");
   Serial.print("\nIP: " + settings.IP.toString() + " || GW: " + settings.GW.toString() + " || SUB: " + settings.SUB.toString());
   Serial.print("\n----------------------------");
@@ -237,6 +238,9 @@ void send_data_mqtt()
   doc_calibration["gyro"] = BNO_calibration.gyro;
   doc_calibration["accel"] = BNO_calibration.accel;
   doc_calibration["mag"] = BNO_calibration.mag;
+  JsonDocument doc_index;
+  doc_index["msdv"] = BNO_data.msdv;
+  doc_index["msi"] = BNO_data.msi;
   char doc_actual_buffer[256];
   serializeJson(doc_actual , doc_actual_buffer);
   client.publish("BNO055/actual", doc_actual_buffer);
@@ -249,6 +253,9 @@ void send_data_mqtt()
   char doc_calibration_buffer[256];
   serializeJson(doc_calibration , doc_calibration_buffer);
   client.publish("BNO055/calibration", doc_calibration_buffer);
+  char doc_index_buffer[256];
+  serializeJson(doc_index , doc_index_buffer);
+  client.publish("BNO055/index", doc_index_buffer);
 }
 void send_float(uint16_t direccion, float valor) {
     union 
@@ -276,6 +283,8 @@ void send_data_modbus_tcp()
   send_float(18, BNO_data.roll_rms);
   send_float(20, BNO_data.accelH_rms);
   send_float(22, BNO_data.accelV_rms);
+  send_float(24, BNO_data.msdv);
+  send_float(26, BNO_data.msi);
 }
 
 void notFound(AsyncWebServerRequest *request) 
@@ -378,19 +387,19 @@ void webserver_setup()
   { 
     if (request->hasParam("pitch")) 
     {
-      BNO_data.pitch_offset = (request->getParam("pitch")->value()).toFloat();
+      BNO_data.pitch_offset += (request->getParam("pitch")->value()).toFloat();
     }
     else if (request->hasParam("roll")) 
     {
-      BNO_data.roll_offset = (request->getParam("roll")->value()).toFloat();
+      BNO_data.roll_offset += (request->getParam("roll")->value()).toFloat();
     }
     else if (request->hasParam("accelH")) 
     {
-      BNO_data.accelH_offset = (request->getParam("accelH")->value()).toFloat();
+      BNO_data.accelH_offset += (request->getParam("accelH")->value()).toFloat();
     }
     else if (request->hasParam("accelV")) 
     {
-      BNO_data.accelV_offset = (request->getParam("accelV")->value()).toFloat();
+      BNO_data.accelV_offset += (request->getParam("accelV")->value()).toFloat();
     }
     else if (request->hasParam("settings")) 
     {
@@ -443,14 +452,14 @@ void setup()
   wifi_setup_ap();
   webserver_setup();
   modbus_tcp_setup();
-  mqtt_setup();
+  //mqtt_setup();
   delay(5000);
 }
 
 void loop()
 {
   mb.task();
-  client.loop();
+  //client.loop();
 
   if ((millis() - timer_last_calibration) > timer_calibration) 
   {
@@ -476,9 +485,9 @@ void loop()
   }
   if ((millis() - timer_mqtt) > settings.mqtt_samplerate) 
   {
-    reconnect();
+    //reconnect();
     timer_mqtt = millis();
-    send_data_mqtt();
+    //send_data_mqtt();
   }
   if ((millis() - timer_pico) > settings.mqtt_samplerate*2) 
   {
